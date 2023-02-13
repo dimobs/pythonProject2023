58cff7a92a204769f0304a7d24f5ab310f7fda13
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-02-13 22:48:07 +0200

    first

---
+++ first_steps_in_coding_lab/01USDtoBGN.py
@@ -0,0 +1,32 @@
+# from math import pi
+#
+# radians = float(input())
+# degrees = radians * 180 / pi
+#
+# print(degrees)
+
+
+# сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
+
+# depozit = float(input())
+# month_depozit = int(input())
+# year_procent = float(input())
+# lihva = depozit * year_procent/100
+# moth_lihva = lihva / 12
+# total = depozit + month_depozit * moth_lihva
+# print (lihva)
+# print(moth_lihva)
+# print(total)
+
+from math import floor
+
+page_count = int(input())
+page_per_hr = int(input())
+day_count = int(input())
+
+total_reading = page_count / page_per_hr
+result = total_reading / day_count
+
+# print(total_reading)
+print(floor(result))
+print(floor(result))
---
+++ first_steps_in_coding_lab/hello_softuni.py
@@ -0,0 +1,35 @@
+# print('Hello Dimo')
+# print('ivan')
+# print(1)
+# print(2)
+# print(3)
+
+# a = int(input())
+# b = int(input())
+#
+# area = a * b
+# print(area)
+
+# inches = float(input())
+# centimeters = inches * 2.54
+# print(centimeters)
+
+# name = input()
+# print('Hello ' + name + '!')
+
+# number = int(input())
+# if number % 2 == 0:
+#     print(f"{number} is Even {number}")
+# else:
+#     print('Odd')
+
+# smfl = float(input())
+# price = smfl * 7.61
+# disc = price * 0.18
+# total = price - disc
+# print(f"The final price is: {total} lv.")
+# print(f"The discount is: {disc} lv")
+
+usd = float(input())
+bgn = usd * 1.79549
+print(bgn)
