8e433ca524f5533993193f50d4350b2123574f97
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-02-19 22:02:14 +0200

    aquarium

--- first_steps_in_coding_lab/01USDtoBGN.py
+++ first_steps_in_coding_lab/01USDtoBGN.py
@@ -1,74 +1,108 @@
-# from math import pi
-#
-# radians = float(input())
-# degrees = radians * 180 / pi
-#
-# print(degrees)
+    # from math import pi
+    #
+    # radians = float(input())
+    # degrees = radians * 180 / pi
+    #
+    # print(degrees)
 
 
-# сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
+    # сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
 
-# depozit = float(input())
-# month_depozit = int(input())
-# year_procent = float(input())
-# lihva = depozit * year_procent/100
-# moth_lihva = lihva / 12
-# total = depozit + month_depozit * moth_lihva
-# print (lihva)
-# print(moth_lihva)
-# print(total)
-#
-# from math import floor
-#
-# page_count = int(input())
-# page_per_hr = int(input())
-# day_count = int(input())
-#
-# total_reading = page_count / page_per_hr
-# result = total_reading / day_count
-#
-# # print(total_reading)
-# print(floor(result))
-#
-#
-#
-# print(floor(result))
+    # depozit = float(input())
+    # month_depozit = int(input())
+    # year_procent = float(input())
+    # lihva = depozit * year_procent/100
+    # moth_lihva = lihva / 12
+    # total = depozit + month_depozit * moth_lihva
+    # print (lihva)
+    # print(moth_lihva)
+    # print(total)
+    #
+    # from math import floor
+    #
+    # page_count = int(input())
+    # page_per_hr = int(input())
+    # day_count = int(input())
+    #
+    # total_reading = page_count / page_per_hr
+    # result = total_reading / day_count
+    #
+    # # print(total_reading)
+    # print(floor(result))
+    #
+    #
+    #
+    # print(floor(result))
 
-# lenght_rank = int(input())
-# with_rank = int(input())
-# heigth_ranll = int(input())
-# precent_ = float(input())/100
-#
-# fill_of_rank = lenght_rank * heigth_ranll * with_rank
-# obem = fill_of_rank / 1000
-# space = obem * (1 - precent_)
-#
-# print(fill_of_rank)
-# print(obem)
-# print(space)
+    # lenght_rank = int(input())
+    # with_rank = int(input())
+    # heigth_ranll = int(input())
+    # precent_ = float(input())/100
+    #
+    # fill_of_rank = lenght_rank * heigth_ranll * with_rank
+    # obem = fill_of_rank / 1000
+    # space = obem * (1 - precent_)
+    #
+    # print(fill_of_rank)
+    # print(obem)
+    # print(space)
 
 
-# from math import pi
+    # from math import pi
+    #
+    # figure = input()
+    # area = 0
+    #
+    # if figure == 'square':
+    #     a = float(input())
+    #     area = a * a
+    #
+    # elif figure == 'rectangle':
+    #     a = float(input())
+    #     b = float(input())
+    #     area = a * b
+    #
+    # elif figure == 'circle':
+    #     r = float(input())
+    #     area = pi * (r ** 2)
+    #
+    # elif figure == 'triangle':
+    #     a = float(input())
+    #     b = float(input())
+    #     area = 1/ 2 * a * b
+    #
+    # print(f'{area:.3f}')
+
+
+    # speed = float(input())
+    # result = ""
+    #
+    # if speed <= 10:
+    #     result = 'slow'
+    #
+    #     elif( 10 < speed >       )
+    #
+    # print(result)
+
+
+
+
+# from forex_python.converter import CurrencyRates
 #
-# figure = input()
-# area = 0
+# c = CurrencyRates()
+# amount = int(input('Enter currency amount: '))
 #
-# if figure == 'square':
-#     a = float(input())
-#     area = a * a
+# from_currency = input('From curerency: ')
+# to_currency = input('To currency: ')
 #
-# elif figure == 'rectangle':
-#     a = float(input())
-#     b = float(input())
-#     area = a * b
+# result = c.convert(from_currency, to_currency, amount)
+# print(f'{result:.2f}')
+
+grade = float(input())
+greade_needed_to_pass = 3
+
+# if grade > 3:
+#     print('Great you pass!')
 #
-# elif figure == 'circle':
-#     r = float(input())
-#     area = pi * (r ** 2)
-#
-# elif figure == 'triangle':
-#     a = float(input())
-#     b = float(input())
-#     area = 1/ 2 * a * b
-#
-# print(f'{area:.3f}')
+#     if grade > 5:
+#         print('Great job!')
