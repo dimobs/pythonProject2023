1aee1f16e8ba77f3a02387a57c7ea31f4b87f888
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-04-14 23:02:50 +0300

    fundamentals

--- fundamentals/orders.py
+++ fundamentals/orders.py
@@ -6,6 +6,15 @@ for _ in range(orders):
     days = int(input())
     capsulesPerDay = int(input())
 
+    if priceCapules < 0.01 or priceCapules > 100:
+        continue
+
+    if days < 1 or days > 31:
+        continue
+
+    if capsulesPerDay < 1 or capsulesPerDay > 2000:
+        continue
+
     order_price = days * capsulesPerDay * capsulesPerDay
     total += order_price
     print(f'The price for the coffee is: ${order_price:.2f}')
---
+++ fundamentals/practic.py
@@ -0,0 +1,17 @@
+# nums_arr = [2, 4, 5, 6, 7, 8, 9]
+#
+# nums = map(lambda x:x + 2, nums_arr)
+# for el in nums:
+#     print(el)
+
+def repeat(word, times):
+    return word * times
+
+text = input()
+n = int(input())
+
+# result = repeat(text, n)
+# print(result)
+
+result = lambda word, times: word * times
+print(result(text, n))
