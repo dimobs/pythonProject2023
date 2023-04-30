2da82beff3e799e7f1cd6d478fec074c9a06f94f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-04-30 21:38:36 +0300

    fundamentals

--- fundamentals/rounding.py
+++ fundamentals/rounding.py
@@ -18,6 +18,14 @@
 #         result.append(char)
 # print(result)
 
+# text = input()
+# result = [char for char in input() if char.lower() not in ['a', 'o', 'u', 'e', 'i']]
+# print("".join(result))
+
+def valid_word(text):
+    result = [char for char in text if char.lower() not in ['a', 'o', 'u', 'e', 'i']]
+    return "".join(result)
 text = input()
-result = [char for char in text if char.lower() not in ['a', 'o', 'u', 'e', 'i']]
-print("".join(result))
+print(valid_word(text))
+
+das
