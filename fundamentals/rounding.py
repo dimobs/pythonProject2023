f4dace28d46feb1eb896a8aebf388e1d2268c57d
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-04-30 21:18:53 +0300

    fundamentals

--- fundamentals/rounding.py
+++ fundamentals/rounding.py
@@ -1,8 +1,11 @@
-def is_even(num):
-    result = num % 2 == 0
-    return result
+# def is_even(num):
+#     result = num % 2 == 0
+#     return result
+#
+# nums =[int(x) for x in input().split()]
+# filtered = list(filter(is_even, nums))
+# for num in filtered:
+#     print(num)
 
-nums =[int(x) for x in input().split()]
-filtered = list(filter(is_even, nums))
-for num in filtered:
-    print(num)
+nums = [ "even" if num % 2 == 0 else 'odd' for num in range(1, 11) ]
+print(nums)
