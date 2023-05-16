d4b37cdf917852df93c35c2b9bb15b5aff5526a2
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-05-16 16:47:24 +0300

    fundamentals

--- fundamentals/classes demo.py
+++ fundamentals/classes demo.py
@@ -26,3 +26,57 @@
 #         emails[index].send()
 #     print(f"{email.sender} says to {email.resive}: S ent: True)
 #
+
+# class Zoo:
+#     __animails = 0
+#
+#     def __init__(self, name):
+#         self.name = name
+#         self.mammals = []
+#         self.fishes = []
+#         self.birts = []
+#
+#     def add_animal(self, species, name):
+#         if species == "mammal":
+#             self.mammals.append(name)
+#         elif species == "fish":
+#             self.fishes.append(name)
+#         elif species == "fish":
+#             self.birts.append(name)
+#
+#         Zoo._Zoo__animails += 1
+#
+#     def get_info(self, species):
+#         info = ""
+#         if species == "mammal":
+#             info = f"Mammals in {self.name}: {', '.join(self.mammals)}"
+#         elif species == "fish":
+#             info = f"Mammals in {self.name}: {', '.join(self.fishes)}"
+#         elif species == "bird":
+#             info = f"Mammals in {self.name}: {', '.join(self.birts)}"
+#
+#         return f"{info}\nTotal animals: {Zoo._Zoo__animails}"
+#
+#
+# name = input()
+# rows = int(input())
+#
+# zoo = Zoo(name)
+#
+# for _ in range(rows):
+#     species, name = input().split()
+#     zoo.add_animal(species, name)
+#
+# species = input()
+# print(zoo.get_info(species))
+
+class Comment:
+    def __init__(self, username, content, likes = 0):
+        self.username = username
+        self.content = content
+        self.likes = likes
+
+comment = Comment("user1", "I like this book")
+print(comment.username)
+print(comment.content)
+print(comment.likes)
