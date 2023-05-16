58eaa4f1948ecf755136aedef956df829c3ae335
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-05-16 17:02:52 +0300

    fundamentals

--- fundamentals/classes demo.py
+++ fundamentals/classes demo.py
@@ -70,13 +70,26 @@
 # species = input()
 # print(zoo.get_info(species))
 
-class Comment:
-    def __init__(self, username, content, likes = 0):
-        self.username = username
-        self.content = content
-        self.likes = likes
+# class Comment:
+#     def __init__(self, username, content, likes = 0):
+#         self.username = username
+#         self.content = content
+#         self.likes = likes
+#
+# comment = Comment("user1", "I like this book")
+# print(comment.username)
+# print(comment.content)
+# print(comment.likes)
 
-comment = Comment("user1", "I like this book")
-print(comment.username)
-print(comment.content)
-print(comment.likes)
+class Party:
+    def __init__(self):
+        self.people = []
+
+party = Party()
+line = input()
+while line != "End":
+    party.people.append(line)
+    line = input()
+
+print(f"Going: {', '.join(party.people)}")
+print(f"Total: {len(party.people)}")
