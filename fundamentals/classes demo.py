26c39de771678919ba60ca202bb33ff809cfedf7
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-05-17 19:20:16 +0300

    fundamentals

--- fundamentals/classes demo.py
+++ fundamentals/classes demo.py
@@ -174,28 +174,31 @@
 # print(zoo.get_info(sortBy))
 
 
-class Circle:
-    __pi = 3.14
+# class Circle:
+#     __pi = 3.14
+#
+#     def __init__(self, diameter):
+#         self.diameter = diameter
+#         self.radius = diameter / 2
+#
+#     def calculate_circumference(self):
+#         return Circle.__pi * self.diameter
+#
+#     def calculate_area(self):
+#         return Circle.__pi * self.radius * self.radius
+#
+#     def calculate_area_of_sector(self, angle):
+#         return Circle.__pi * (angle/360) * self.radius * self.radius
+#
+#
+# circle = Circle(10)
+# angle = 5
+#
+# print(f"{circle.calculate_circumference():.2f}")
+# print(f"{circle.calculate_area():.2f}")
+# print(f"{circle.calculate_area_of_sector(angle):.2f}")
 
-    def __init__(self, diameter):
-        self.diameter = diameter
-        self.radius = diameter / 2
-
-    def calculate_circumference(self):
-        return Circle.__pi * self.diameter
-
-    def calculate_area(self):
-        return Circle.__pi * self.radius * self.radius
-
-    def calculate_area_of_sector(self, angle):
-        return Circle.__pi * (angle/360) * self.radius * self.radius
 
 
-circle = Circle(10)
-angle = 5
-
-print(f"{circle.calculate_circumference():.2f}")
-print(f"{circle.calculate_area():.2f}")
-print(f"{circle.calculate_area_of_sector(angle):.2f}")
 
 
